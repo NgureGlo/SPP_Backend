4c1a14aab05059937339cac8fb6876d659fc886c
ngureglo <ngmweru@gmail.com>
2024-03-10 23:54:27 +0300

    added more functionalities

--- app/models.py
+++ app/models.py
@@ -5,20 +5,35 @@ from datetime import datetime
 import json
 from sqlalchemy_serializer import SerializerMixin
 
-# sign up table 
+# user table 
 
 bcrypt = Bcrypt()
 
+class Role(Enum):
+    Administrator = 'Administrator'
+    Educator = 'Educator'
+
 class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.Enum(Role), nullable=False)
     name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    photo_url = db.Column(db.String(150), nullable=True)
 
-    def __init__(self, name, email, password):
+    def __init__(self, role, name, email, password):
+        self.role = role
         self.name = name
         self.email = email
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'role': str(self.role.value),
+            'name': str(self.name),
+            'email': str(self.email)
+        }
 
 # add course table 
 
@@ -35,7 +50,7 @@ class CourseLevel(Enum):
 class Course(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     course_level = db.Column(db.Enum(CourseLevel), nullable=False)
-    course_id = db.Column(db.String(20), unique=True, nullable=False)
+    course_code = db.Column(db.String(20), unique=True, nullable=False)
     course_name = db.Column(db.String(100), nullable=False)
     added_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     added_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -43,16 +58,31 @@ class Course(db.Model, SerializerMixin):
     deletion_date = db.Column(db.DateTime)
 
     def __repr__(self):
-        return f"Course(course_level={self.course_level}, course_id={self.course_id}, course_name={self.course_name})"
+        return f"Course(course_level={self.course_level}, course_code={self.course_code}, course_name={self.course_name})"
     
     def serialize(self):
         return {
             'id': self.id,
             'course_level': str(self.course_level),
-            'course_id': str(self.course_id),
+            'course_code': str(self.course_code),
             'course_name': str(self.course_name),
             'added_by': str(self.added_by),
             'added_date': str(self.added_date),
             'deleted_by': str(self.deleted_by),
             'deletion_date': str(self.deletion_date)
+        }
+
+# add students table
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_reg_no = db.Column(db.String(20), unique=True, nullable=False)
+    student_name = db.Column(db.String(100), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'student_reg_no': str(self.student_reg_no),
+            'student_name': str(self.student_name),
+            'course_id': str(self.course_id)
         }
--- app/routes.py
+++ app/routes.py
@@ -4,15 +4,16 @@ from email_validator import validate_email, EmailNotValidError
 from flask_bcrypt import Bcrypt
 from app.models import User
 from app.models import Course
+from app.models import Student
 
 
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({ 'message': 'welcome to flask app' })
 
-# define endpoints for registration
-@app.route('/register', methods=['GET', 'POST'])
-def register():
+# define endpoints for registering admin
+@app.route('/register_admin', methods=['GET', 'POST'])
+def register_admin():
     data = request.json
 
     name = str(data['name'])
@@ -36,7 +37,43 @@ def register():
 
     try:
 
-        user = User(name=name, email=email, password=password)
+        user = User(role='Administrator', name=name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({'message': 'Your account has been created.'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}) 
+
+# define endpoints for registration
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    data = request.json
+
+    
+    name = str(data['name'])
+    email = str(data['email'])
+    password = str(data['password'])
+
+    # Check if the email is valid
+    try:
+        v = validate_email(email)
+        email = v["email"]
+    except EmailNotValidError as e:
+        
+        return jsonify({'message': 'Invalid email address.'})
+
+    # Check password length
+    if len(password) < 6:
+        
+        return jsonify({'message': 'Password must be at least 6 characters long.'})
+    
+    bcrypt = Bcrypt()
+
+    try:
+
+        user = User(role='Educator', name=name, email=email, password=password)
         db.session.add(user)
         db.session.commit()
 
@@ -60,11 +97,55 @@ def login():
 
     if user and bcrypt.check_password_hash(user.password, password):
             # Login successful, redirect to dashboard or home page
+            auth_result = {'uid': user.id, 'role': str(user.role.value)}
            
-            return jsonify({'meessage': 'Login successful!', 'uid' : user.id})
+            return jsonify({'meessage': 'Login successful!', 'data' : auth_result})
     else:
             # Login failed, show error message
             return jsonify({'message': 'Invalid email or password.'})
+    
+# define endpoints for viewing users
+@app.route('/view_users', methods=['GET'])
+def view_user():
+    user= User.query.all()
+    user_json = [User.serialize(record) for record in user]
+    return jsonify({'data': user_json})
+
+# define endpoints for deleting a user
+@app.route('/user/delete/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    # Query the user to delete
+    user = User.query.get_or_404(user_id)
+
+    # Delete the user from the database
+    db.session.delete(user)
+    db.session.commit()
+
+    # message to confirm deletion
+    return jsonify ({'message': 'User deleted successfully!'})
+
+# define endpoints for updating a user
+@app.route('/update_user/<int:user_id>', methods=['GET', 'POST'])
+def update_user(user_id):
+    data = request.json
+
+    # Query the user to find user
+    user = User.query.get_or_404(user_id)
+
+    name = str(data['name'])
+    photo = str(data['photo'])
+
+    user.name=name
+    user.photo=photo
+
+    try:
+        db.session.commit()
+
+        return jsonify ({'message': 'Updated successfully!'})
+    
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
     
 
 # define endpoints for viewing a course
@@ -74,22 +155,20 @@ def courses():
     courses_json = [Course.serialize(record) for record in courses]
     return jsonify({'data': courses_json})
 
-
-
 # define endpoints for adding a course
 @app.route('/add_course', methods=['GET', 'POST'])
 def add_course():
     data = request.json
     print (str(data))
 
-    course_id = str(data['course_id'])
+    course_code = str(data['course_code'])
     course_level = str(data['course_level'])
     course_name = str(data['course_name'])
     current_user_id = int(data['current_user_id'])
     
-    if course_id and course_level and course_name:
+    if course_code and course_level and course_name:
         added_by = current_user_id  # Assuming current_user is the logged-in user
-        course = Course(course_id=course_id, course_level=course_level, course_name=course_name, added_by=added_by)
+        course = Course(course_code=course_code, course_level=course_level, course_name=course_name, added_by=added_by)
         db.session.add(course)
         db.session.commit()
 
@@ -111,6 +190,61 @@ def delete_course(course_id):
 
     # message to confirm deletion
     return jsonify ({'message': 'Course deleted successfully!'})
+
+# define endpoints for adding a student
+@app.route('/add_student', methods=['POST'])
+def add_student():
+    data = request.json
+    print (str(data))
+
+    student_reg_no = str(data['student_reg_no'])
+    student_name = str(data['student_name'])
+    course_id = str(data['course_id'])
+    
+    if student_reg_no and student_name and course_id:
+        student = Student(student_reg_no=student_reg_no, student_name=student_name, course_id=course_id)
+        db.session.add(student)
+        db.session.commit()
+
+        return jsonify ({'message': 'Student added successfully!'})
+    
+    else:
+
+        return jsonify({'message': 'Unable to add Student. Please fill in all sections.'})
+    
+# define endpoints for viewing a student
+@app.route('/view_students', methods=['GET'])
+def view_students():
+    student= Student.query.all()
+    student_json = [Student.serialize(record) for record in student]
+    return jsonify({'data': student_json})
+
+# define endpoints for viewing a student added by a specific lecturer
+@app.route('/lecturer_students/<int:lecturer_id>', methods=['GET'])
+def lecturer_students(lecturer_id):
+    student= Student.query.filter(Student.lecturer == lecturer_id)
+    student_json = [Student.serialize(record) for record in student]
+    return jsonify({'data': student_json})
+
+# define endpoints for deleting a student
+@app.route('/student/delete/<int:student_id>', methods=['POST'])
+def delete_student(student_id):
+    # Query the student to delete
+    student = Student.query.get_or_404(student_id)
+
+    # Delete the student from the database
+    db.session.delete(student)
+    db.session.commit()
+
+    # message to confirm deletion
+    return jsonify ({'message': 'Student deleted successfully!'})
+
+# define endpoints for viewing a student from a specific courses
+@app.route('/course_students/<int:course_id>', methods=['GET'])
+def course_students(course_id):
+    student= Student.query.filter(Student.course_id == course_id)
+    student_json = [Student.serialize(record) for record in student]
+    return jsonify({'data': student_json})
 
 # define endpoints for prediction
 @app.route('/predict', methods=['POST'])
