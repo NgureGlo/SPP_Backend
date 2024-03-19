1dcb94cefc36966982d610f10a2770768c361986
ngureglo <ngmweru@gmail.com>
2024-03-19 10:10:16 +0300

    Reverted to linear regresson model

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ CORS(app)  # Enable CORS for all routes
 resources={r"/add_course": {"origins": "http://127.0.0.1:5501"}}
 
 # load model to app
-with open('support_vector_machine_model.pkl', 'rb') as model_file:
+with open('linear_regression_model.pkl', 'rb') as model_file:
     loaded_model = pickle.load(model_file)
 
 from app import routes
